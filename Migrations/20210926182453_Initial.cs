32a0938f439b0f85f45e0fd4147c49cc2f5447e7
Carlos J Figueroa <71535916+carlosmdq44@users.noreply.github.com>
2021-09-29 13:30:58 -0300

    Add files via upload

---
+++ Migrations/20210926182453_Initial.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RentCars.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cars",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Mark = table.Column<int>(type: "INTEGER", nullable: false),
+                    Model = table.Column<string>(type: "TEXT", nullable: true),
+                    NumberOfDoors = table.Column<int>(type: "INTEGER", nullable: false),
+                    Colour = table.Column<int>(type: "INTEGER", nullable: false),
+                    ChangeBook = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cars", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cars");
+        }
+    }
+}
